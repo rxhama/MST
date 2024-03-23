cd8d52a8e32557a459e9f199fef020d31e575dba
rxhama <rxhama02@gmail.com>
2024-03-23 10:14:19 +0000

    Added reverse-delete and (weak) degree constrained kruskals

--- script.js
+++ script.js
@@ -42,13 +42,20 @@ class AlgoController {
         if (index < 0 || index >= this.steps.length) return;
 
         const step = this.steps[index];
-        Object.entries(step.changes).forEach(([id, classNames]) => {
+        Object.entries(step.change.changes).forEach(([id, classNames]) => {
             classNames.forEach(className => {
                 if (className == 'outlined') {
-                    cy1.$(`#${id}`).flashClass(className);
+                    if (step.change.add) {
+                        cy1.$(`#${id}`).flashClass(className);
+                    }
                 }
                 else {
-                    cy1.$(`#${id}`).addClass(className);
+                    if (step.change.add) {
+                        cy1.$(`#${id}`).addClass(className);
+                    }
+                    else {
+                        cy1.$(`#${id}`).removeClass(className);
+                    }
                 }
             })
         });
@@ -59,9 +66,13 @@ class AlgoController {
     undoStep(index) {
         if (index < 0 || index >= this.steps.length) return;
         const step = this.steps[index];
-        Object.keys(step.changes).forEach(id => {
-            console.log(`${id}: ${step.changes[id]}`);
-            cy1.$(`#${id}`).removeClass(step.changes[id]);
+        Object.keys(step.change.changes).forEach(id => {
+            if (step.change.add) {
+                cy1.$(`#${id}`).removeClass(step.change.changes[id]);
+            }
+            else {
+                cy1.$(`#${id}`).addClass(step.change.changes[id]);
+            }
         })
         if (index < 1) {
             this.updateDisplays(this.steps[0]);
@@ -189,7 +200,7 @@ function primsAlgorithm(graph) {
     let currNode = graph.nodes(':selected');
     unvisitedNodes = unvisitedNodes.difference(currNode);
 
-    // Sorting initial edge queue by weight
+    // Sorting initial edge queue by weight - ascending order
     const initAdjacentEdges = currNode.connectedEdges().filter(edge => !visitedEdges.contains(edge) && (unvisitedNodes.contains(edge.source()) != unvisitedNodes.contains(edge.target())));
     edgeQueue = edgeQueue.union(initAdjacentEdges);
     edgeQueue = edgeQueue.sort(function(a, b) {
@@ -198,10 +209,12 @@ function primsAlgorithm(graph) {
 
     // Initial step
     const initStep = {};
-    initStep.changes = {};
-    initStep.changes[currNode.data('id')] = ['chosen'];
+    initStep.change = {};
+    initStep.change.add = true;
+    initStep.change.changes = {};
+    initStep.change.changes[currNode.data('id')] = ['chosen'];
     edgeQueue.forEach(edge => {
-        initStep.changes[edge.data('id')] = ['search'];
+        initStep.change.changes[edge.data('id')] = ['search'];
     });
     initStep.edgeQueue = initAdjacentEdges.map(edge => `${edge.data('id')} (${edge.data('weight')})`);
     initStep.mstCost = mstCost;
@@ -247,11 +260,13 @@ function primsAlgorithm(graph) {
 
         step.mstCost = mstCost;
         step.edgeQueue = edgeQueue.map(edge => `${edge.data('id')} (${edge.data('weight')})`);
-        step.changes = {};
-        step.changes[nextEdge.data('id')] = ['chosen'];
-        step.changes[nextNode.data('id')] = ['chosen'];
+        step.change = {};
+        step.change.add = true;
+        step.change.changes = {};
+        step.change.changes[nextEdge.data('id')] = ['chosen'];
+        step.change.changes[nextNode.data('id')] = ['chosen'];
         adjacentEdges.forEach(edge => {
-            step.changes[edge.data('id')] = ['search'];
+            step.change.changes[edge.data('id')] = ['search'];
         });
 
         steps.push(step);
@@ -278,7 +293,7 @@ function kruskalsAlgorithm(graph) {
         groupDict[nodeId] = groupDict[nodeId].union(node);
     });
 
-    // Sorting edge queue by weight
+    // Sorting edge queue by weight - ascending order
     let edgeQueue = graph.edges();
     edgeQueue = edgeQueue.sort(function(a, b) {
         return a.data('weight') - b.data('weight')
@@ -286,7 +301,9 @@ function kruskalsAlgorithm(graph) {
 
     // Init step with 0 changes, 0 cost, just displaying the initial edgeQueue
     const initStep = {};
-    initStep.changes = {};
+    initStep.change = {};
+    initStep.change.add = true;
+    initStep.change.changes = {};
     initStep.mstCost = mstCost;
     initStep.edgeQueue = edgeQueue.map(edge => `${edge.data('id')} (${edge.data('weight')})`);
     steps.push(initStep);
@@ -317,8 +334,10 @@ function kruskalsAlgorithm(graph) {
         mstCost += nextEdge.data('weight');
         step.mstCost = mstCost;
         step.edgeQueue = edgeQueue.map(edge => `${edge.data('id')} (${edge.data('weight')})`);
-        step.changes = {};
-        step.changes[nextEdge.data('id')] = ['chosen'];
+        step.change = {};
+        step.change.add = true;
+        step.change.changes = {};
+        step.change.changes[nextEdge.data('id')] = ['chosen'];
         
         // Getting source and target nodes of edge
         const sourceNode = nextEdge.source();
@@ -326,12 +345,12 @@ function kruskalsAlgorithm(graph) {
         if (unvisitedNodes.contains(sourceNode)) {
             unvisitedNodes = unvisitedNodes.difference(sourceNode);
 
-            step.changes[sourceNode.data('id')] = ['chosen'];
+            step.change.changes[sourceNode.data('id')] = ['chosen'];
         }
         if (unvisitedNodes.contains(targetNode)) {
             unvisitedNodes = unvisitedNodes.difference(targetNode);
 
-            step.changes[targetNode.data('id')] = ['chosen'];
+            step.change.changes[targetNode.data('id')] = ['chosen'];
         }
 
         steps.push(step);
@@ -339,6 +358,108 @@ function kruskalsAlgorithm(graph) {
     console.log("KRUSKALS COMPLETED!!!");
     algoController.setSteps(steps);
 }
+
+function reverseDeleteAlgorithm(graph) {
+    const steps = [];
+    let mstCost = graph.edges().reduce((sum, edge) => {
+        return sum + edge.data('weight');
+    }, 0); // Cost of minimum spanning tree at each step
+    
+    let edgeCount = graph.edges().length;
+
+    // Since all edges are chosen initially, we can track of if they're
+    // chosen here, by id. This is to avoid actually removing edges
+    // from the chosen class before the search is done and therefore
+    // avoid the edge changing colour before we even know if we want
+    // to remove that edge
+    let edgeInGraph = {};
+    graph.edges().forEach(edge => {
+        edgeInGraph[edge.data('id')] = true;
+    });
+
+    // Sort edge queue by weight - descending order
+    let edgeQueue = graph.edges();
+    edgeQueue = edgeQueue.sort(function(a, b) {
+        return b.data('weight') - a.data('weight')
+    });
+
+    const initStep = {};
+    initStep.change = {};
+    initStep.change.add = true;
+    initStep.change.changes = {};
+    initStep.mstCost = mstCost;
+    initStep.edgeQueue = edgeQueue.map(edge => `${edge.data('id')} (${edge.data('weight')})`);
+    graph.elements().forEach(ele => {
+        initStep.change.changes[ele.data('id')] = ['chosen'];
+    });
+    steps.push(initStep);
+
+    while (edgeCount > graph.nodes().length - 1) {
+        const step = {};
+
+        // Choosing edge
+        let nextEdge = null;
+        while (nextEdge == null) {
+            const edge = edgeQueue[0];
+            const sourceId = edge.source().data('id');
+            const targetId = edge.target().data('id');
+            edgeInGraph[edge.data('id')] = false;
+
+            let chosenEdges = graph.edges().filter(edge => edgeInGraph[edge.data('id')]);
+            let connectedNodes = chosenEdges.connectedNodes();
+            let subgraph = chosenEdges.union(connectedNodes);
+
+            let sourceNodeCollection = subgraph.bfs({
+                roots: `#${sourceId}`,
+                directed: false
+            }).path.nodes();
+
+            let targetNodeCollection = subgraph.bfs({
+                roots: `#${targetId}`,
+                directed: false
+            }).path.nodes();
+
+            if (!sourceNodeCollection.intersection(targetNodeCollection).empty()) {
+                nextEdge = edge;
+                edgeCount--;
+                
+                break;
+            }
+            edgeInGraph[edge.data('id')] = true;
+            edgeQueue = edgeQueue.difference(edge);
+        }
+        edgeQueue = edgeQueue.difference(nextEdge);
+        
+        mstCost -= nextEdge.data('weight');
+        step.mstCost = mstCost;
+        step.edgeQueue = edgeQueue.map(edge => `${edge.data('id')} (${edge.data('weight')})`);
+        step.change = {};
+        step.change.add = false;
+        step.change.changes = {};
+        step.change.changes[nextEdge.data('id')] = ['chosen'];
+
+        steps.push(step);
+    }
+    console.log("REVERSE DELETE COMPLETED!!!");
+    algoController.setSteps(steps);
+}
+
+// // Does a BFS search from startingNode but only goes along edges with the 'chosen' class
+// // Returns that subgraph of startingNode ()
+// function customBfsSearch(graph, startingNodeId) {
+//     // Create a collection that includes only chosen edges and the nodes they are connected to
+//     let chosenEdges = graph.edges('.chosen');
+//     let connectedNodes = chosenEdges.connectedNodes();
+//     let subgraph = chosenEdges.union(connectedNodes);
+
+//     // BFS to get all nodes in the startingNodes subgraph
+//     let traversal = subgraph.bfs({
+//         roots: `#${startingNodeId}`,
+//         directed: false
+//     });
+
+//     return traversal.path.nodes();
+// }
 
 function boruvkasAlgorithm(graph) {
     const steps = [];
@@ -357,7 +478,9 @@ function boruvkasAlgorithm(graph) {
 
     // Init step with 0 changes, 0 cost, just displaying the initial node groups
     const initStep = {};
-    initStep.changes = {};
+    initStep.change = {};
+    initStep.change.add = true;
+    initStep.change.changes = {};
     initStep.mstCost = mstCost;
     initStep.edgeQueue = Object.values(groupDict).map(group => group.filter(ele => ele.isNode()).map(node => node.data('id')));
     steps.push(initStep);
@@ -366,12 +489,14 @@ function boruvkasAlgorithm(graph) {
         let visited = graph.collection();
         for (let key in groupDict) {
             const step = {};
-            step.changes = {};
+            step.change = {};
+            step.change.add = true;
+            step.change.changes = {};
             
             // Flashes the entire group of nodes
             const nodesInGroup = groupDict[key].filter(ele => ele.isNode()).map(node => node.data('id'));
             nodesInGroup.forEach(nodeId => {
-                step.changes[nodeId] = ['outlined'];
+                step.change.changes[nodeId] = ['outlined'];
             });
 
             
@@ -402,28 +527,28 @@ function boruvkasAlgorithm(graph) {
             const targetId = nextEdge.target().data('id');
             
             
-            if (!step.changes[nextEdge.data('id')]) {
-                step.changes[nextEdge.data('id')] = ['chosen'];
+            if (!step.change.changes[nextEdge.data('id')]) {
+                step.change.changes[nextEdge.data('id')] = ['chosen'];
             }
             else {
-                step.changes[nextEdge.data('id')].push('chosen');
+                step.change.changes[nextEdge.data('id')].push('chosen');
             }
 
             if (groupDict[sourceId].length == 1) {
-                if (!step.changes[sourceId]) {
-                    step.changes[sourceId] = ['chosen'];
+                if (!step.change.changes[sourceId]) {
+                    step.change.changes[sourceId] = ['chosen'];
                 }
                 else {
-                    step.changes[sourceId].push('chosen');
+                    step.change.changes[sourceId].push('chosen');
                 }
             }
 
             if (groupDict[targetId].length == 1) {
-                if (!step.changes[targetId]) {
-                    step.changes[targetId] = ['chosen'];
+                if (!step.change.changes[targetId]) {
+                    step.change.changes[targetId] = ['chosen'];
                 }
                 else {
-                    step.changes[targetId].push('chosen');
+                    step.change.changes[targetId].push('chosen');
                 }
             }
             
@@ -463,6 +588,98 @@ function boruvkasAlgorithm(graph) {
         }
     }
     console.log("BORUVKAS COMPLETED!!!");
+    algoController.setSteps(steps);
+}
+
+function degreeConstrainedKruskals(graph, maxDegree) {
+    const steps = [];
+    let mstCost = 0;
+
+    let unvisitedNodes = graph.nodes();
+    const targetEdgeCount = unvisitedNodes.length - 1;
+    let edgeCount = 0;
+
+    let groupDict = {};
+    unvisitedNodes.forEach(node => {
+        const nodeId = node.data('id');
+        groupDict[nodeId] = graph.collection();
+        groupDict[nodeId] = groupDict[nodeId].union(node);
+    });
+
+    let edgeQueue = graph.edges();
+    edgeQueue = edgeQueue.sort(function(a, b) {
+        return a.data('weight') - b.data('weight')
+    });
+
+    const initStep = {};
+    initStep.change = {};
+    initStep.change.add = true;
+    initStep.change.changes = {};
+    initStep.mstCost = mstCost;
+    initStep.edgeQueue = edgeQueue.map(edge => `${edge.data('id')} (${edge.data('weight')})`);
+    steps.push(initStep);
+
+    let edgeInGraph = {};
+    graph.edges().forEach(edge => {
+        edgeInGraph[edge.data('id')] = false;
+    });
+    
+    while (edgeCount < targetEdgeCount) {
+        const step = {};
+
+        let nextEdge = null;
+        while (nextEdge == null) {
+            if (edgeQueue.empty()) {
+                console.log("No more edges to choose from");
+                return;
+            }
+
+            const edge = edgeQueue[0];
+            const sourceId = edge.source().data('id');
+            const targetId = edge.target().data('id');
+            // CHECK THIS IF STATEMENT LOGICCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
+            if (graph.$(`#${sourceId}`).connectedEdges().filter(edge => !edgeInGraph[`#${edge.data('id')}`]).length + 1 <= maxDegree && graph.$(`#${targetId}`).connectedEdges().filter(edge => !edgeInGraph[`#${edge.data('id')}`]).length + 1 <= maxDegree) {
+                if (groupDict[sourceId].intersection(groupDict[targetId]).empty()) {
+                    nextEdge = edge;
+                    edgeCount++;
+                    edgeInGraph[edge.data('id')] = true;
+                    groupDict[sourceId] = groupDict[sourceId].union(groupDict[targetId]);
+                    groupDict[sourceId].forEach(function(node) {
+                        groupDict[node.data('id')] = groupDict[sourceId];
+                    });
+
+                    break;
+                }
+            }
+            edgeQueue = edgeQueue.difference(edge);
+        }
+        edgeQueue = edgeQueue.difference(nextEdge);
+
+        mstCost += nextEdge.data('weight');
+        step.mstCost = mstCost;
+        step.edgeQueue = edgeQueue.map(edge => `${edge.data('id')} (${edge.data('weight')})`);
+        step.change = {};
+        step.change.add = true;
+        step.change.changes = {};
+        step.change.changes[nextEdge.data('id')] = ['chosen'];
+        
+        // Getting source and target nodes of edge
+        const sourceNode = nextEdge.source();
+        const targetNode = nextEdge.target();
+        if (unvisitedNodes.contains(sourceNode)) {
+            unvisitedNodes = unvisitedNodes.difference(sourceNode);
+
+            step.change.changes[sourceNode.data('id')] = ['chosen'];
+        }
+        if (unvisitedNodes.contains(targetNode)) {
+            unvisitedNodes = unvisitedNodes.difference(targetNode);
+
+            step.change.changes[targetNode.data('id')] = ['chosen'];
+        }
+
+        steps.push(step);
+    }
+    console.log("DEGREE CONSTRAINED KRUSKALS COMPLETED!!!");
     algoController.setSteps(steps);
 }
 
@@ -513,6 +730,12 @@ function start() {
     }
     else if (selectedAlgo == 'boruvkas') {
         boruvkasAlgorithm(cy1);
+    }
+    else if (selectedAlgo == 'reverse-delete') {
+        reverseDeleteAlgorithm(cy1);
+    }
+    else if (selectedAlgo == 'dckruskals') {
+        degreeConstrainedKruskals(cy1, 1);
     }
 };
 
