9fcc2b718bb54268e0c510381bdb09cef746617e
rxhama <rxhama02@gmail.com>
2024-03-31 17:03:59 +0100

    Added cytoscape to dev dependencies for testing

--- utils.test.js
+++ utils.test.js
@@ -1,8 +1,23 @@
+const cytoscape = require('cytoscape');
 const { primsAlgorithm } = require('./utils');
+
+let cy = cytoscape({
+    elements: {
+        nodes: [
+            { data: { id: 'a' } },
+            { data: { id: 'b' } }
+
+        ],
+        edges: [
+            { data: { id: 'ab', source: 'a', target: 'b', weight: 2 } }
+        ]
+    }
+});
 
 describe('primsAlgorithm', () => {
     test('testing primsAlgorithm', () => {
-        const result = 2;
-        expect(result).toEqual(2);
+        const expectedResult = 2;
+        const result = cy.edges()[0].data('weight');
+        expect(result).toEqual(expectedResult);
     });
 });
