b1def26c4401ecbfb0f44eabbfb65e5d1d5c496a
rxhama <rxhama02@gmail.com>
2024-03-31 16:54:44 +0100

    Added testing functionality, use npm test

---
+++ utils.test.js
@@ -0,0 +1,8 @@
+const { primsAlgorithm } = require('./utils');
+
+describe('primsAlgorithm', () => {
+    test('testing primsAlgorithm', () => {
+        const result = 2;
+        expect(result).toEqual(2);
+    });
+});
